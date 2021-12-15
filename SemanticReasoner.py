97108348abb90153bc23db6c1c832e6c63bc1508
MazurkiewiczMatt <mateusz.a.mazurkiewicz@gmail.com>
2021-12-15 18:50:42 +0100

    Add IDs to tutorials

--- SemanticReasoner.py
+++ SemanticReasoner.py
@@ -4,8 +4,11 @@ object_lists = [Software.Software, Tutorials.Tutorials, Fields.Fields]
 def FetchObject(ID, search_lists=object_lists):
     for search_list in search_lists:
         for s_object in search_list:
-            if s_object["ID"] == ID:
-                return s_object
+            try:
+                if s_object["ID"] == ID:
+                    return s_object
+            except KeyError:
+                print("Detected an object without ID")
     return 0
 
 
