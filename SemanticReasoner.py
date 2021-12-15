eb8936cab5a49861ca7295de87b90c659d38a984
MazurkiewiczMatt <mateusz.a.mazurkiewicz@gmail.com>
2021-12-15 19:22:04 +0100

    Add branchOf and requirements to semantic reasoner

--- SemanticReasoner.py
+++ SemanticReasoner.py
@@ -8,8 +8,38 @@ def FetchObject(ID, search_lists=object_lists):
                 if s_object["ID"] == ID:
                     return s_object
             except KeyError:
-                print("Detected an object without ID")
+                print("ERROR: detected an object without ID")
     return 0
 
+def BranchOf(object, depth=0):
+    branch = [object]
+    if 'BranchOf' in object.keys():
+        parent = FetchObject(object["BranchOf"])
+        if parent == 0:
+            branch.append({'ID': object["BranchOf"]})
+            return branch
+        else:
+            branch.append(parent)
+        if depth==1:
+            return branch
+        else:
+            branch += BranchOf(parent, depth=depth-1)[1:]
+    return branch
 
-print(FetchObject("FourierTransform"))
+def Requirements(object, depth=0):
+    requirements = []
+    if 'Requires' in object.keys():
+        requirements_IDs = object["Requires"]
+        if type(requirements_IDs) is not list:
+            requirements_IDs = [requirements_IDs]
+        for requirement_ID in requirements_IDs:
+            requirement = FetchObject(requirement_ID)
+            if requirement == 0:
+                requirements.append(requirement_ID)
+            else:
+                requirements.append(requirement)
+                if not(depth==1):
+                    requirements += Requirements(requirement, depth=depth-1)
+    return requirements
+
+print(BranchOf(FetchObject("SignalProcessing"), depth=1))
